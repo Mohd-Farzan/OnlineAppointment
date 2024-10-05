7d6dfb75055016d803d2f63fa4eb697dcc0cc76f
Faiz Ali <sayyedfaiz336@gmail.com>
2024-10-05 11:47:05 +0530

    Initial commit

---
+++ home.js
@@ -0,0 +1,56 @@
+const express =require("express");
+const app=express();
+const path=require("path");
+app.set("view engine","e.js");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const userModel=require("./model/contol")
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'model')));
+
+// remoe after create
+
+app.get("/try",function(req,res){
+  res.render("adil.ejs");
+})
+
+app.get("/",function(req,res){
+    res.render("home.ejs");
+})
+
+
+ app.get("/show",function(req,res){
+    res.render("create.ejs")
+})
+app.post("/create" , async (req,res)=>{
+    let {name,title,image}=req.body 
+   let u= await userModel.create({
+      name,
+      title,
+      image
+    });
+    res.send(u)
+  })
+  app.get("/read" , async (req,res)=>{
+    let user=await userModel.find();
+    res.render('doc.ejs',{user});
+  })
+  
+app.get("/user",function(req,res){
+    res.render("profile.ejs");
+})
+
+app.get("/chat",function(req,res){
+    res.render("chat.ejs");
+})
+
+app.get("/view",function(req,res){
+    res.render("chatuser.ejs");
+})
+
+
+
+app.get("/appointment",function(req,res){
+  res.render("appointment.ejs");
+})
+app.listen(3000);
